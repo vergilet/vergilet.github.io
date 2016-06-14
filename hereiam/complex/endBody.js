50846f824ec9e1bdbbca24e2511a1e6874cc9f3b
YaroslavO <yaroslavo@softwareplanetgroup.com>
2016-06-14 13:25:32 +0300

    Here I  am project was added

---
+++ hereiam/complex/endBody.js
@@ -0,0 +1,2 @@
+document.writeln("<script src='complex/js/app.js' type='text/javascript'></script>");
+document.writeln("<script src='complex/js/chat.js' type='text/javascript'></script>");
---
+++ hereiam/complex/loadMapAndFunctions.js
@@ -0,0 +1,1 @@
+$("body").append("<script type='text/javascript' src='complex/js/map.js' ></script>");
